d666c95bbfd3d8ce7cbfd92a37afe3cef80f3c18
Senpai_Desi <72793345+SenpaiDesi@users.noreply.github.com>
2023-01-16 14:34:43 +0100

    ip tracker

--- config.py
+++ config.py
@@ -3,6 +3,14 @@ import os
 import subprocess
 import json
 import requests
+import main
+from ProgramFIles import iptrack
+
+def print_banner():
+    print(Fore.RED + "+-+-+-+-+-+-+\n|d|o|k|k|T|X|\n+-+-+-+-+-+-+\n")
+    print(Fore.RESET + "\n")
+    print("Welcome to DokkTX, the termux adaptation of DokkCLi. \n")
+        
 def start():
     os.system("clear")
     hostname = subprocess.check_output("whoami")
@@ -14,8 +22,8 @@ def start():
         os.system("clear")
         print(Fore.RED + "+-+-+-+-+-+-+\n|d|o|k|k|T|X|\n+-+-+-+-+-+-+\n")
         print(Fore.RESET + "\n")
-        print("Welcome to dokkcli, if this is your first time using this software. Please use module 1 Setup. \n")
-        pass
+        print("Welcome to DokkTX, the termux adaptation of DokkCLi. \n")
+
     else:
         os.system("clear")
         print(Fore.RED + "Authentication failed. Some common reasons:\n")
@@ -23,9 +31,31 @@ def start():
         print("2: License key used from another device\n")
         print("3: Piracy\n")
         print("Try again later.")
-        os.system("sleep 20")
+        os.system("sleep 10")
         exit()
 
 
+# update menu
+def update():
+    os.system("apt-get update -y")
+    os.system("apt-get upgrade -y")
+    os.system("pkg install python3")
+    os.system("pip install colorama")
+    os.system("pip install requests")
+    os.system("pip install subprocess")
+    os.system("pip install pygeoip")
+    os.system("clear")
+    print(Fore.GREEN + "Update successfull!\n")
+    os.system("sleep 10")
+    main.main()
 
+
+def hack_menu_one():
+    os.system('clear')
+    print_banner()
+    menu_select = input(Fore.GREEN + "[1]    ipinfo.\n[2]    Remote shell\n[3]    Port Scanner\n[4]    Show networks and info\n[5]    Soon to come\n[6]    Soon to come.\n\n")
+    if str(menu_select) == "1":
+        iptrack.track()
+    if str(menu_select) == "2":
+        os.system('clear')
 
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import config
+from colorama import Fore
 import os
-def main():
-    config.start()
 
-    
+def launch():
+    config.start()
+    main()
+
+def main():
+    global menu_select
+    print(Fore.RED + "+-+-+-+-+-+-+\n|d|o|k|k|T|X|\n+-+-+-+-+-+-+\n")
+    print(Fore.RESET + "\n")
+    print("Welcome to DokkTX, the termux adaptation of DokkCLi. \n\n")
+    menu_select = input(Fore.GREEN + "[1]    Update.\n[2]    tools and scripts\n[3]    Global Tools\n[4]    To be made\n[5]    Information.\n[0]    Exit\n\n")
+    # Showcase menu
+
+    if str(menu_select) == "1":
+        config.update()
+    elif str(menu_select) == "2":
+
+
 
 
 
@@ -12,4 +27,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    launch()
