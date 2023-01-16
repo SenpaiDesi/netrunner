96f3c426871a497b0b6d7e66945b730814e4cc14
Senpai_Desi <72793345+SenpaiDesi@users.noreply.github.com>
2023-01-16 15:09:59 +0100

    Cleanup

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def launch():
 
 def main():
     global menu_select
+    os.system('clear')
     config.print_banner()
     menu_select = input(Fore.GREEN + "[1]    Update.\n[2]    tools and scripts\n[3]    Global Tools\n[4]    To be made\n[5]    Information.\n[0]    Exit\n\n")
     # Showcase menu
@@ -15,6 +16,8 @@ def main():
     if str(menu_select) == "1":
         config.update()
     elif str(menu_select) == "2":
+        os.system('clear')
+        config.print_banner()
         hack_menu_select = input(Fore.GREEN + "[1]    Network tools and attacks.\n[2]    Password cracking\n[3]    Page downloader\n[4]    Soon to come.\n[5]    Soon to come.\n[0]    Exit\n\n")
         if str(hack_menu_select) == "1":
             config.hack_menu_one()
